03173d7c92ba0729a5061b8a986e40aa7aa10806
pranavoj <pranavojale@gmail.com>
2021-03-25 14:10:25 +0530

    Adding project code

---
+++ src/test/java/com/ojale/pranav/camelmicroserviceb/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojale.pranav.camelmicroserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
